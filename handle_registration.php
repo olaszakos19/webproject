3c1ff2218d1bb6a6b42f9dd9e4dadc7083f80d94
olaszakos19 <akos.olasz17@gmail.com>
2024-01-02 15:32:19 +0100

    final

--- handle_registration.php
+++ handle_registration.php
@@ -3,24 +3,30 @@
 require('connect.php');
 
 
-$kapcsolat
-    ->prepare("INSERT INTO users (username, passwd) VALUES (:felhasznalonev, :jelszo)")
-    ->execute([
-        'felhasznalonev' => $_POST["username"],
-        'jelszo' => md5($_POST["password"]),
-    ]);
+$checkUsernameQuery = $kapcsolat->prepare("SELECT COUNT(*) as count FROM users WHERE username = :felhasznalonev");
+$checkUsernameQuery->execute(['felhasznalonev' => $_POST["username"]]);
+$result = $checkUsernameQuery->fetch(PDO::FETCH_ASSOC);
 
+if ($result['count'] > 0) {
+    header("Location: reg_failed.php");
+} else {
 
-$newUserId = $kapcsolat->lastInsertId();
+    $kapcsolat
+        ->prepare("INSERT INTO users (username, passwd) VALUES (:felhasznalonev, :jelszo)")
+        ->execute([
+            'felhasznalonev' => $_POST["username"],
+            'jelszo' => md5($_POST["password"]),
+        ]);
 
+    $newUserId = $kapcsolat->lastInsertId();
 
-$kapcsolat
-    ->prepare("INSERT INTO stats (userID) VALUES (:user_id)")
-    ->execute([
-        'user_id' => $newUserId,
-    ]);
+    $kapcsolat
+        ->prepare("INSERT INTO stats (userID) VALUES (:user_id)")
+        ->execute([
+            'user_id' => $newUserId,
+        ]);
 
-
-
-
-header("Location: login.php");
+    header("Location: login.php");
+    exit(); 
+}
+?>
